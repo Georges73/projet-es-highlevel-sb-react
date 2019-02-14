2b3e215c098a98a5714ee891466d461229dc0214
vanhoutteachille@gmail.com <vanhoutteachille@gmail.com>
2019-02-14 15:17:18 +0100

    first commit

---
+++ back-end/achille/src/main/java/be/es/sbreact/achille/AchilleApplication.java
@@ -0,0 +1,14 @@
+package be.es.sbreact.achille;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AchilleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AchilleApplication.class, args);
+	}
+
+}
+

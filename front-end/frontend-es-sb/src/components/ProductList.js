85922b2538dc62d726e738bef6930b06b7c7ffe4
vanhoutteachille@gmail.com <vanhoutteachille@gmail.com>
2019-02-18 15:17:11 +0100

    first commit with react

--- front-end/frontend-es-sb/src/components/ProductList.js
+++ front-end/frontend-es-sb/src/components/ProductList.js
@@ -1,42 +1,42 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
- class ProductList extends Component {
+class Productlist extends Component {
 
     constructor(props) {
         super(props);
         this.state = { products: []};
-      }
+      } 
 
       componentDidMount() {
         fetch('http://localhost:8080/products/findAll')
         .then((response) => response.json()) 
         .then((responseData) => { 
           this.setState({ 
-            products: responseData._embedded.products,
+            cars: responseData._embedded.products,
           }); 
         })
         .catch(err => console.error(err)); 
       }
-
-  render() {
-    const tableRows = this.state.products.map
-    ((product, index) => 
-    <tr key={index}>
-      <td>{product.id}</td>
-      <td>{product.title}</td>
-      <td>{product.description}</td>
-      <td>{product.manufacturer}</td>
-      <td>{product.price}</td>
-    </tr>
-  );
-  return (
-    <div className="App">
-      <table>
-        <tbody>{tableRows}</tbody>
-      </table>
-    </div>
-  );
-
-  }
+  
+      render() {
+        const tableRows = this.state.products.map((product, index) => 
+          <tr key={index}>
+            <td>{product.id}</td>
+            <td>{product.title}</td>
+            <td>{product.description}</td>
+            <td>{product.manufacturer}</td>
+            <td>{product.price}</td>
+          </tr>
+        );
+      
+        return (
+          <div className="App">
+            <table>
+              <tbody>{tableRows}</tbody>
+            </table>
+          </div>
+        );
+      }
 }
-export default  ProductList;
+
+export default Productlist;
